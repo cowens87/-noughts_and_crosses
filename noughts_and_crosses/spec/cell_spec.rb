a543ad6d096e7ae7e504d38602628d73e168fdd8
Cydnee Owens <cks410@yahoo.com>
2021-04-08 16:03:21 -0700

    add empty, place_piece and render method

--- noughts_and_crosses/spec/cell_spec.rb
+++ noughts_and_crosses/spec/cell_spec.rb
@@ -5,6 +5,9 @@ require './lib/piece'
 RSpec.describe Cell do
   before(:each) do
     @cell = Cell.new('1')
+    @cell1 = Cell.new('2')
+    @cell2 = Cell.new('3')
+    @cell3 = Cell.new('4')
     @x = Piece.new('X')
     @o = Piece.new('O')
   end
@@ -12,5 +15,17 @@ RSpec.describe Cell do
   it 'exists' do
     expect(@cell).to be_a Cell
     expect(@cell.space).to eq '1'
+  end
+
+  it 'can find if a piece is in the cell' do
+    expect(@cell.piece).to be_nil
+    expect(@cell.empty?).to be true
+  end
+
+  it 'can place a piece' do
+    @cell.place_piece(@x)
+
+    expect(@cell.piece).to eq @x
+    expect(@cell.empty?).to be false
   end
 end
